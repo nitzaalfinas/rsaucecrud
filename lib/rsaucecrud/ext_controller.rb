6b44a93995b8ea614718e7986e4b7a7bf56c2d02
Nitza Alfinas <nitzaalfinas@gmail.com>
2016-11-16 16:49:35 +0700

    perbaikan controller new

--- lib/rsaucecrud/ext_controller.rb
+++ lib/rsaucecrud/ext_controller.rb
@@ -126,7 +126,7 @@ def create_controller_create(the_namespace, the_controller, the_model, the_field
 
     @tfs = ''
     the_fields.each do |tf|
-        if tf != 'id' || tf != 'created_at' || tf != 'updated_at'
+        if tf.to_s != 'id' || tf.to_s != 'created_at' || tf.to_s != 'updated_at'
             @tfs = @tfs + ', ' + tf.to_s + ': params[:' + the_model.downcase + '][:' + tf.to_s + ']'
         end
     end
