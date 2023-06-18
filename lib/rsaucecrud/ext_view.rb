d00f208c26776ff56fbe7db437cb403cb1e3a209
Nitza Alfinas <nitzaalfinas@gmail.com>
2023-06-19 06:28:11 +0700

    perbaikan

--- lib/rsaucecrud/ext_view.rb
+++ lib/rsaucecrud/ext_view.rb
@@ -16,16 +16,6 @@ def create_html_index(the_namespace, the_controller, the_model, the_fields)
     dir_name = Rails.root.to_s + '/app/views/' + the_namespace.downcase + '/' + the_controller.downcase
     FileUtils.mkdir_p dir_name
 
-    # order
-    @ths = ''
-    the_fields.each do |thx|
-        @ths = @ths + '<tr>
-            <td>
-                <%= show_th(\'' + thx.to_s + '\', \'' + thx.to_s + '\', @urlfor, @rows, @sort, @order, @filter_rules).html_safe %>
-            </td>
-        </tr>' + "\n"
-    end
-
     # table head column
     @table_head_th = ''
     the_fields.each do |tdx|
@@ -35,7 +25,9 @@ def create_html_index(the_namespace, the_controller, the_model, the_fields)
           <%= advsearch_table_th_nav('#{tdx.to_s}', '#{tdx.to_s}', 'text').html_safe %>
         </th>"
     end
-    @table_head = "<thead id=\"head_search\"><tr>#{@table_head_th}</tr></thead>"
+    @table_head = "<thead id=\"head_search\">
+    <tr>#{@table_head_th}</tr>
+    </thead>"
 
     # Data
     @datas_td = "<td>
@@ -60,133 +52,133 @@ def create_html_index(the_namespace, the_controller, the_model, the_fields)
     File.open(dir_name + '/' + @the_file_name, 'w') { |f|
 
       f.write "
-      <!-- TIPE GRID -->
-      <div class=\"content-wrapper\">
-        <section class=\"content-header\">
-          <h1>
-            #{the_controller}
-          </h1>
-          <%= app_sp_breadcrumb([
-            {icon: 'fa fa-dashboard', text: '', link: '/dashboard/index' },
-            {icon: '', text: '#{the_controller}', link: '#' },
-          ]).html_safe %>
-        </section>
+<!-- TIPE GRID -->
+<div class=\"content-wrapper\">
+  <section class=\"content-header\">
+    <h1>
+      #{the_controller}
+    </h1>
+    <%= app_sp_breadcrumb([
+      {icon: 'fa fa-dashboard', text: '', link: '/dashboard/index' },
+      {icon: '', text: '#{the_controller}', link: '#' },
+    ]).html_safe %>
+  </section>
 
-        <!-- Main content -->
-        <section class=\"content\">
-          <div class=\"row\">
+  <!-- Main content -->
+  <section class=\"content\">
+    <div class=\"row\">
 
-            <!-- NAVIGASI HALAMAN -->
-            <div class=\"col-xs-12\" style=\"margin-bottom: 0px;\">
-              <table style=\"float: right\">
-                <tr>
-                  <td style=\"padding: 0 5px;\">
-                    <% if @filter_rules != '[]' %>
-                      <%= link_to 'Reset Pencarian', {action: 'index', page: @page, rows: @rows, sort: @sort, order: @order, filter_rules: \"[]\"}, {class: ''} %>
-                    <% end %>
-                  </td>
+      <!-- NAVIGASI HALAMAN -->
+      <div class=\"col-xs-12\" style=\"margin-bottom: 0px;\">
+        <table style=\"float: right\">
+          <tr>
+            <td style=\"padding: 0 5px;\">
+              <% if @filter_rules != '[]' %>
+                <%= link_to 'Reset Pencarian', {action: 'index', page: @page, rows: @rows, sort: @sort, order: @order, filter_rules: \"[]\"}, {class: ''} %>
+              <% end %>
+            </td>
 
-                  <td style=\"padding: 0 5px;\">
-                    <a href=\"<%= url_for(action: 'new') %>\" class=\"btn btn-primary\">Add New</a>
-                  </td>
-                  
-                  <td style=\"padding: 0 5px;\">
-                    <nav aria-label=\"Page navigation\">
-                      <ul class=\"pagination\">
-                        <% if ((@page.to_i - 1) != 0) %>
-                          <li>
-                            <a href=\"<%= @urlfor %>?page=<%= @page.to_i - 1 %>&amp;rows=<%= @rows %>&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
-                              <span aria-hidden=\"true\">&laquo;</span>
-                            </a>
-                          </li>
-                        <% end %>
-                        <% if ((@page.to_i + 1) < (@total_page + 1)) %>
-                          <li>
-                            <a href=\"<%= @urlfor %>?page=<%= @page.to_i + 1 %>&amp;rows=<%= @rows %>&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
-                              <span aria-hidden=\"true\">&raquo;</span>
-                            </a>
-                          </li>
-                        <% end %>
-                      </ul>
-                    </nav>
-                  </td>
-                  <td style=\"padding: 0 5px;\">
-                    <div class=\"btn-group\">
-                      <button type=\"button\" class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
-                        Baris:
-                        <%= @rows %>
-                      </button>
+            <td style=\"padding: 0 5px;\">
+              <a href=\"<%= url_for(action: 'new') %>\" class=\"btn btn-primary\">Add New</a>
+            </td>
+            
+            <td style=\"padding: 0 5px;\">
+              <nav aria-label=\"Page navigation\">
+                <ul class=\"pagination\">
+                  <% if ((@page.to_i - 1) != 0) %>
+                    <li>
+                      <a href=\"<%= @urlfor %>?page=<%= @page.to_i - 1 %>&amp;rows=<%= @rows %>&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
+                        <span aria-hidden=\"true\">&laquo;</span>
+                      </a>
+                    </li>
+                  <% end %>
+                  <% if ((@page.to_i + 1) < (@total_page + 1)) %>
+                    <li>
+                      <a href=\"<%= @urlfor %>?page=<%= @page.to_i + 1 %>&amp;rows=<%= @rows %>&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
+                        <span aria-hidden=\"true\">&raquo;</span>
+                      </a>
+                    </li>
+                  <% end %>
+                </ul>
+              </nav>
+            </td>
+            <td style=\"padding: 0 5px;\">
+              <div class=\"btn-group\">
+                <button type=\"button\" class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                  Baris:
+                  <%= @rows %>
+                </button>
 
-                      <ul class=\"dropdown-menu\">
-                        <li>
-                          <a href=\"<%= @urlfor %>?page=1&amp;rows=5&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
-                            5
-                          </a>
-                        </li>
-                        <li>
-                          <a href=\"<%= @urlfor %>?page=1&amp;rows=10&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
-                            10
-                          </a>
-                        </li>
-                        <li>
-                          <a href=\"<%= @urlfor %>?page=1&amp;rows=15&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
-                            15
-                          </a>
-                        </li>
-                        <li>
-                          <a href=\"<%= @urlfor %>?page=1&amp;rows=20&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
-                            20
-                          </a>
-                        </li>
-                        <li>
-                          <a href=\"<%= @urlfor %>?page=1&amp;rows=30&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
-                            30
-                          </a>
-                        </li>
-                        <li>
-                          <a href=\"<%= @urlfor %>?page=1&amp;rows=50&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
-                            50
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </td>
-                </tr>
-              </table>
-            </div>
-            <!-- NAVIGASI HALAMAN -->
-
-            <div class=\"col-xs-12\">
-              <div class=\"box\">
-                <!-- /.box-header -->
-                <div class=\"box-body table-responsive\">
-
-                  <!-- Table grid -->
-                  <table class=\"table table-striped\">
-                    #{@table_head}
-                    <tbody>
-                      <% @datas.each do |b| %>
-                        <tr>
-                          #{@datas_td}
-                        </tr>
-                      <% end %>
-                    </tbody>
-                  </table>
-                  <!--/ Table grid -->
-
-                </div>
+                <ul class=\"dropdown-menu\">
+                  <li>
+                    <a href=\"<%= @urlfor %>?page=1&amp;rows=5&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
+                      5
+                    </a>
+                  </li>
+                  <li>
+                    <a href=\"<%= @urlfor %>?page=1&amp;rows=10&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
+                      10
+                    </a>
+                  </li>
+                  <li>
+                    <a href=\"<%= @urlfor %>?page=1&amp;rows=15&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
+                      15
+                    </a>
+                  </li>
+                  <li>
+                    <a href=\"<%= @urlfor %>?page=1&amp;rows=20&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
+                      20
+                    </a>
+                  </li>
+                  <li>
+                    <a href=\"<%= @urlfor %>?page=1&amp;rows=30&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
+                      30
+                    </a>
+                  </li>
+                  <li>
+                    <a href=\"<%= @urlfor %>?page=1&amp;rows=50&amp;sort=<%= @sort %>&amp;order=<%= @order %>&amp;filter_rules=<%= @filter_rules %>\">
+                      50
+                    </a>
+                  </li>
+                </ul>
               </div>
-            </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <!-- NAVIGASI HALAMAN -->
+
+      <div class=\"col-xs-12\">
+        <div class=\"box\">
+          <!-- /.box-header -->
+          <div class=\"box-body table-responsive\">
+
+            <!-- Table grid -->
+            <table class=\"table table-striped\">
+              #{@table_head}
+              <tbody>
+                <% @datas.each do |b| %>
+                  <tr>
+                    #{@datas_td}
+                  </tr>
+                <% end %>
+              </tbody>
+            </table>
+            <!--/ Table grid -->
 
           </div>
-        </section>
+        </div>
       </div>
 
-      <script type=\"text/javascript\">
-      $(document).ready(function () {
-        adding_search_head_value_for_filtering_and_go(`<%= @filter_rules.html_safe %>`);
-      });
-      </script>
+    </div>
+  </section>
+</div>
+
+<script type=\"text/javascript\">
+$(document).ready(function () {
+  adding_search_head_value_for_filtering_and_go(`<%= @filter_rules.html_safe %>`);
+});
+</script>
       "
 
   }
