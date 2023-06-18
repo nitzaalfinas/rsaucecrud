814535a4790ddfa99d4d540074c8ec35cd16f8f4
Nitza Alfinas <nitzaalfinas@gmail.com>
2023-06-18 19:54:02 +0700

    Update ext_controller.rb

--- lib/rsaucecrud/ext_controller.rb
+++ lib/rsaucecrud/ext_controller.rb
@@ -32,83 +32,19 @@ end
 def create_controller_index(the_namespace, the_controller, the_model, the_fields)
 
 "
+  advanced_page_and_filter(#{the_model}.column_names)
 
-    def index
-        if params[:page]
-            @page = params[:page].to_i
-        else
-            @page = 1
-        end
+  if @keluaran_length != 0
+    @total_item = #{the_model}.where(@nu_where).size
+    @total_page = (@total_item.to_f / @rows.to_i).ceil
+   @datas = #{the_model}.where(@nu_where).order(@sort + ' ' + @order).limit(@rows).offset(@startx)
+  else
+    @total_item = #{the_model}.all.size
+    @total_page = (@total_item.to_f / @rows.to_i).ceil
+    @datas = #{the_model}.all.order(@sort + ' ' + @order).limit(@rows).offset(@startx)
+  end
 
-        if params[:rows]
-            if params[:rows].to_i > 50
-                @rows = 50
-            else
-                @rows = params[:rows].to_i
-            end
-        else
-            @rows = 10
-        end
-
-        if params[:sort]
-            @sort = params[:sort]
-        else
-            @sort = 'id'
-        end
-
-        if params[:order]
-            @order = params[:order]
-        else
-            @order = 'desc'
-        end
-
-        if params[:filter_rules] == nil
-            @filter_rules = '[]'
-        elsif params[:filter_rules] == ''
-            @filter_rules = '[]'
-        elsif params[:filter_rules] == '[]'
-            @filter_rules = '[]'
-        else
-            @filter_rules = params[:filter_rules]
-        end
-
-        @startx = (@page - 1) * @rows
-
-        @parsed_json = JSON.parse @filter_rules
-
-        @keluaran = ''
-        @parsed_json.each do |njes|
-            if njes['op'] == 'equal'
-                @keluaran = @keluaran+' '+njes['field']+' = \\''+njes['value']+'\\' and '
-            elsif njes['op'] == 'not equal'
-                @keluaran = @keluaran+' '+njes['field']+' <> \\''+njes['value']+'\\' and '
-            elsif njes['op'] == 'contains'
-                @keluaran = @keluaran+' '+njes['field']+' like \\'%'+njes['value']+'%\\' and '
-            elsif njes['op'] == 'l_contains'
-                @keluaran = @keluaran+' '+njes['field']+' like \\''+njes['value']+'%\\' and '
-            elsif njes['op'] == 'less'
-                @keluaran = @keluaran+' '+njes['field']+' < \\''+njes['value']+'\\' and '
-            elsif njes['op'] == 'greater'
-                @keluaran = @keluaran+' '+njes['field']+' > \\''+njes['value']+'\\' and '
-            end
-        end
-        @keluaran_length = @keluaran.length
-
-        if @keluaran_length != 0
-            @keluaran_where =  @keluaran[0,@keluaran_length - 4]
-
-            @total_item = " + the_model + ".where(@keluaran_where).size
-            @total_page = (@total_item.to_f/@rows.to_i).ceil
-            @datas = " + the_model + ".where(@keluaran_where).order(@sort+' '+@order).limit(@rows).offset(@startx)
-        else
-
-            @total_item = " + the_model + ".all.size
-            @total_page = (@total_item.to_f/@rows.to_i).ceil
-            @datas = " + the_model + ".all.order(@sort+' '+@order).limit(@rows).offset(@startx)
-        end
-
-        @urlfor = url_for({controller: '/" + the_namespace.downcase + "/" + the_controller.downcase + "', action: 'index'})
-    end
+  @urlfor = url_for(action: 'index')
 "
 end
 
