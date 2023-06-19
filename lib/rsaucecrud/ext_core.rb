76c2e6dddd6fa8c4a7771770fd784cd4f73b3d31
Nitza Alfinas <nitzaalfinas@gmail.com>
2023-06-19 07:47:09 +0700

    perbaikan

--- lib/rsaucecrud/ext_core.rb
+++ lib/rsaucecrud/ext_core.rb
@@ -1,4 +1,17 @@
 def rsaucecrud(the_namespace, the_controller, the_model, the_fields)
   create_controller_all(the_namespace, the_controller, the_model, the_fields)
   create_html_view(the_namespace, the_controller, the_model, the_fields)
+
+  puts "
+  resource :#{the_controller.downcase} do
+    collection do
+      get    '/'                => '#{the_controller.downcase}#index'
+      get    'new'              => '#{the_controller.downcase}#new'
+      post   'create'           => '#{the_controller.downcase}#create'
+      get    ':id/edit'         => '#{the_controller.downcase}#edit'
+      patch  ':id/update'       => '#{the_controller.downcase}#update'
+      delete ':id/delete'       => '#{the_controller.downcase}#delete'
+    end
+  end
+"
 end
--- lib/rsaucecrud/version.rb
+++ lib/rsaucecrud/version.rb
@@ -1,3 +1,3 @@
 module Rsaucecrud
-  VERSION = '0.1.34'
+  VERSION = '0.1.35'
 end
