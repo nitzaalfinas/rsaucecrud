a151e7d8ebddd6661f5ce185afe94f2ef18d5edf
Nitza Alfinas <nitzaalfinas@gmail.com>
2023-06-19 07:58:13 +0700

    perbaikan

--- lib/rsaucecrud/ext_core.rb
+++ lib/rsaucecrud/ext_core.rb
@@ -3,6 +3,8 @@ def rsaucecrud(the_namespace, the_controller, the_model, the_fields)
   create_html_view(the_namespace, the_controller, the_model, the_fields)
 
   puts "
+
+  # === route ===
   resource :#{the_controller.downcase} do
     collection do
       get    '/'                => '#{the_controller.downcase}#index'
@@ -13,5 +15,13 @@ def rsaucecrud(the_namespace, the_controller, the_model, the_fields)
       delete ':id/delete'       => '#{the_controller.downcase}#delete'
     end
   end
+
+  # === menu ===
+            <li class=\"<%= @menu_#{the_controller.downcase} %>\">
+              <a href=\"<%= url_for(controller: '/#{the_controller.downcase}', action: 'index') %>\">
+                <i class=\"fa fa-user\"></i>
+                <span>#{the_controller}</span>
+              </a>
+            </li>
 "
 end
